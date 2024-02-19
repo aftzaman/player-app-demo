3dcad8daf4d01f537f838db9fe1337c2c7573885
Habib <aftzaman@gmail.com>
2024-02-19 10:36:25 +0400

    Modified Appstart to app.js

--- app.js
+++ app.js
@@ -19,45 +19,9 @@ router.use(function (req, res, next) {
     next();
 });
 var socket = require('./app/utill/socket_io');
-socket.onScheduleModified(function(){
-    console.log('onScheduleModified on *: ' + port);
-})
-
 
 const server = app.listen(port, () => {
     console.log('listening on *: ' + port);
 });
 
 console.log('Magic happens on port ' + port);
-
-
-// const mqtt = require('mqtt')
-// const protocol = 'mqtt'
-// const host = '127.0.0.1'
-// const port2 = '1883'
-// const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
-
-// const connectUrl = `${protocol}://${host}:${port2}`
-
-
-// const client = mqtt.connect(connectUrl, {
-//     clientId,
-//     clean: true,
-//     connectTimeout: 4000,
-//     username: 'emqx',
-//     password: 'public',
-//     reconnectPeriod: 1000,
-//   })
-  
-// const topic = 'test'
-
-// client.on('connect', () => {
-//       client.subscribe([topic], () => {
-//     console.log(`Subscribe to topic '${topic}'`)
-//   })
-   
-//   })
-  
-// client.on('message', (topic, payload) => {
-//     console.log('Received Message:', topic, payload.toString())
-//   })
